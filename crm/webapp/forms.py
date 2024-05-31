c07ec0d6a10d651c044fc639443cc43bb60cd56a
maheshkmp <maheshpererakm@gmail.com>
2024-05-31 16:43:45 +0530

    Initial commit

---
+++ crm/webapp/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+
+# - register/create a user
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+
+        model = User
+        fields = [ 'username', 'password1', 'password2']
+
+# - Login a user
+
+class Loginform(AuthenticationForm):
+    
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
