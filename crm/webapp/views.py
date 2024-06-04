8b2483e234570da67da94265e5e8b33f3b2b9147
maheshkmp <maheshpererakm@gmail.com>
2024-06-04 09:31:02 +0530

    Authentication settings and dashboard

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 
 from.forms import CreateUserForm, Loginform
 
+from django.contrib.auth.models import auth 
+from django.contrib.auth import authenticate
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here
 
 def home(request):
@@ -21,9 +26,62 @@ def register (request):
             
             form.save()
 
-            #return redirect('')
+            return redirect("my-login")
 
 
     context = { 'form' : form}
 
     return render(request, 'webapp/register.html', context=context)
+
+
+# - login a user
+
+def my_login(request):
+    
+    form = Loginform()
+
+    if request.method == "POST":
+        
+        form = Loginform(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("dashboard")
+
+    context = {'form' : form}
+
+    return render(request, 'webapp/my-login.html', context = context)
+
+
+
+# - Dashboard
+
+@login_required(login_url='my-login')
+
+def dashboard(request):
+
+    return render(request, 'webapp/dashboard.html')
+
+
+
+
+
+# - User logout
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect("my-login")
+            
+
+        
