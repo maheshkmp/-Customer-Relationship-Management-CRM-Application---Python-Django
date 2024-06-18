e7356ef31764d4df8ce350a709e680bb57f920a7
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-06-18 23:18:46 +0530

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'webapp',
     'crispy_forms',
 ]
@@ -110,7 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = ' static/'
-
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
